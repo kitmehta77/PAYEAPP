cbe3ec0f0d6e87b682dfcedf83f8844e9414a022
Lei <gabriel_sl19798@hotmail.com>
2021-03-21 15:49:18 +1300

    Try loader

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', static_url_path='', static_folder='static')
+@app.route('/', static_url_path='', static_folder='')
 def hello_world():
     return 'Hello World!'
 
