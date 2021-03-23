f6cf4d3f605c6e1aae5a96992c87e6004a792fdb
kitmehta77 <kit.mehta@gmail.com>
2021-03-24 08:42:13 +1300

    repush

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/getNetIncome/', method=['POST'])
+
 def netIncome():
     gross_income = request.get_json()
     print(gross_income)
     gross = float(gross_income["gross_income"])
     return jsonify(gross - PAYE.totalTax(gross))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
